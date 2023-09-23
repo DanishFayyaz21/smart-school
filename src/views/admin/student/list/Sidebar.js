f812330df0e9f45208a081df02cd1087a157af53
danishfayyaz21 <danishfayyaz91@gmail.com>
2023-09-23 03:48:52 -0700

    updated code

--- src/views/admin/student/list/Sidebar.js
+++ src/views/admin/student/list/Sidebar.js
@@ -18,41 +18,22 @@ import { Button, Label, FormText, Form, Input } from 'reactstrap'
 // ** Store & Actions
 import { addUser } from '../store'
 import { useDispatch } from 'react-redux'
+import { post } from '../../../../services'
 
 const defaultValues = {
   email: '',
   contact: '',
-  company: '',
-  fullName: '',
+
   username: '',
-  country: null
+  section: null
 }
 
-const countryOptions = [
-  { label: 'Australia', value: 'Australia' },
-  { label: 'Bangladesh', value: 'Bangladesh' },
-  { label: 'Belarus', value: 'Belarus' },
-  { label: 'Brazil', value: 'Brazil' },
-  { label: 'Canada', value: 'Canada' },
-  { label: 'China', value: 'China' },
-  { label: 'France', value: 'France' },
-  { label: 'Germany', value: 'Germany' },
-  { label: 'India', value: 'India' },
-  { label: 'Indonesia', value: 'Indonesia' },
-  { label: 'Israel', value: 'Israel' },
-  { label: 'Italy', value: 'Italy' },
-  { label: 'Japan', value: 'Japan' },
-  { label: 'Korea', value: 'Korea' },
-  { label: 'Mexico', value: 'Mexico' },
-  { label: 'Philippines', value: 'Philippines' },
-  { label: 'Russia', value: 'Russia' },
-  { label: 'South', value: 'South' },
-  { label: 'Thailand', value: 'Thailand' },
-  { label: 'Turkey', value: 'Turkey' },
-  { label: 'Ukraine', value: 'Ukraine' },
-  { label: 'United Arab Emirates', value: 'United Arab Emirates' },
-  { label: 'United Kingdom', value: 'United Kingdom' },
-  { label: 'United States', value: 'United States' }
+const SectionOptions = [
+  { label: 'A', value: 'A' },
+  { label: 'B', value: 'B' },
+  { label: 'C', value: 'C' },
+  { label: 'D', value: 'D' },
+  { label: 'F', value: 'F' }
 ]
 
 const checkIsValid = data => {
@@ -62,8 +43,8 @@ const checkIsValid = data => {
 const SidebarNewUsers = ({ open, toggleSidebar }) => {
   // ** States
   const [data, setData] = useState(null)
-  const [plan, setPlan] = useState('basic')
-  const [role, setRole] = useState('subscriber')
+  const [status, setStatus] = useState()
+  const [studentclass, setClass] = useState()
   const [formStatus, setFormStatus] = useState(1)
 
   // ** Store Vars
@@ -79,49 +60,52 @@ const SidebarNewUsers = ({ open, toggleSidebar }) => {
   } = useForm({ defaultValues })
 
   // ** Function to handle form submit
-  const onSubmit = data => {
-    setData(data)
-    if (checkIsValid(data)) {
-      if (formStatus == 2)toggleSidebar()
-      dispatch(
-        addUser({
-          role,
-          avatar: '',
-          status: 'active',
-          email: data.email,
-          currentPlan: plan,
-          billing: 'auto debit',
-          company: data.company,
-          contact: data.contact,
-          fullName: data.fullName,
-          username: data.username,
-          country: data.country.value
-        })
-      )
-      setFormStatus(2)
+  const onSubmit = async (data) => {
+    const formData = { ...data, status, studentclass, role: "Student" }
+    const res = await post("/register-student", formData)
+    console.log("res.....................", res)
+    // setData(data)
+    // if (checkIsValid(data)) {
+    //   if (formStatus == 2)toggleSidebar()
+    //   dispatch(
+    //     addUser({
+    //       role,
+    //       avatar: '',
+    //       status: 'active',
+    //       email: data.email,
+    //       currentPlan: plan,
+    //       billing: 'auto debit',
+    //       company: data.company,
+    //       contact: data.contact,
+    //       fullName: data.fullName,
+    //       username: data.username,
+    //       country: data.country.value
+    //     })
+    //   )
+    //   setFormStatus(2)
 
-    } else {
-      for (const key in data) {
-        if (data[key] === null) {
-          setError('country', {
-            type: 'manual'
-          })
-        }
-        if (data[key] !== null && data[key].length === 0) {
-          setError(key, {
-            type: 'manual'
-          })
-        }
-      }
-    }
+    // } else {
+    //   for (const key in data) {
+    //     if (data[key] === null) {
+    //       setError('country', {
+    //         type: 'manual'
+    //       })
+    //     }
+    //     if (data[key] !== null && data[key].length === 0) {
+    //       setError(key, {
+    //         type: 'manual'
+    //       })
+    //     }
+    //   }
+    // }
   }
 
   const handleSidebarClosed = () => {
     for (const key in defaultValues) {
       setValue(key, '')
     }
-    setRole('subscriber')
-    setPlan('basic')
+    // setRole('subscriber')
+    // setPlan('basic')
   }
 
   return (
@@ -138,20 +122,34 @@ const SidebarNewUsers = ({ open, toggleSidebar }) => {
         <Form onSubmit={handleSubmit(onSubmit)}>
           <div><h1>Student details</h1></div>
           <div className='mb-1'>
-            <Label className='form-label' for='fullName'>
-              Full Name <span className='text-danger'>*</span>
-            </Label>
-            <Controller
-              name='fullName'
-              control={control}
-              render={({ field }) => (
-                <Input id='fullName' placeholder='M John' invalid={errors.fullName && true} {...field} />
-              )}
-            />
+            <div>
+              <Label className='form-label' for='fullName'>
+                Fisrt Name <span className='text-danger'>*</span>
+              </Label>
+              <Controller
+                name='firstName'
+                control={control}
+                render={({ field }) => (
+                  <Input id='fullName' placeholder='M John' invalid={errors.fullName && true} {...field} />
+                )}
+              />
+            </div>
+            <div>
+              <Label className='form-label' for='fullName'>
+                Last Name <span className='text-danger'>*</span>
+              </Label>
+              <Controller
+                name='lastName'
+                control={control}
+                render={({ field }) => (
+                  <Input id='fullName' placeholder='M John' invalid={errors.fullName && true} {...field} />
+                )}
+              />
+            </div>
           </div>
           <div className='mb-1'>
             <Label className='form-label' for='username'>
-              Father Name <span className='text-danger'>*</span>
+              Username <span className='text-danger'>*</span>
             </Label>
             <Controller
               name='username'
@@ -194,30 +192,51 @@ const SidebarNewUsers = ({ open, toggleSidebar }) => {
             />
           </div>
           <div className='mb-1'>
-            <Label className='form-label' for='company'>
+            <Label className='form-label' for='cnic'>
               CNIC/B-form <span className='text-danger'>*</span>
             </Label>
             <Controller
-              name='company'
+              name='cnic'
               control={control}
               render={({ field }) => (
-                <Input id='company' placeholder='Company Pvt Ltd' invalid={errors.company && true} {...field} />
+                <Input id='cnic' placeholder='00000-0000000-0' invalid={errors.company && true} {...field} />
               )}
             />
           </div>
+
           <div className='mb-1'>
-            <Label className='form-label' for='country'>
+            <Label className='form-label' for='user-role'>
+              Class
+            </Label>
+            <Input type='select' id='user-role' name='user-role' value={studentclass} onChange={e => setClass(e.target.value)}>
+              <option value='1st Class'>1st Class</option>
+              <option value='2nd Class'>2nd Class</option>
+              <option value='3rd Class'>3rd Class</option>
+              <option value='4th Class'>4th Class</option>
+              <option value='5th Class'>5th Class</option>
+              <option value='6th Class'>6th Class</option>
+              <option value='7th Class'>7th Class</option>
+              <option value='8th Class'>8th Class</option>
+              <option value='9th Class'>9th Class</option>
+              <option value='10th Class'>10th Class</option>
+              <option value='11th Class'>11th Class</option>
+              <option value='12th Class'>12th Class</option>
+            </Input>
+          </div>
+
+          <div className='mb-1'>
+            <Label className='form-label' for='section'>
               Section <span className='text-danger'>*</span>
             </Label>
             <Controller
-              name='country'
+              name='section'
               control={control}
               render={({ field }) => (
                 // <Input id='country' placeholder='Australia' invalid={errors.country && true} {...field} />
                 <Select
                   isClearable={false}
                   classNamePrefix='select'
-                  options={countryOptions}
+                  options={SectionOptions}
                   theme={selectThemeColors}
                   className={classnames('react-select', { 'is-invalid': data !== null && data.country === null })}
                   {...field}
@@ -225,27 +244,13 @@ const SidebarNewUsers = ({ open, toggleSidebar }) => {
               )}
             />
           </div>
-          <div className='mb-1'>
-            <Label className='form-label' for='user-role'>
-              Class
-            </Label>
-            <Input type='select' id='user-role' name='user-role' value={role} onChange={e => setRole(e.target.value)}>
-              <option value='subscriber'>Subscriber</option>
-              <option value='editor'>Editor</option>
-              <option value='maintainer'>Maintainer</option>
-              <option value='author'>Author</option>
-              <option value='admin'>Admin</option>
-            </Input>
-          </div>
-          <div className='mb-1' value={plan} onChange={e => setPlan(e.target.value)}>
+          <div className='mb-1'  >
             <Label className='form-label' for='select-plan'>
               Status
             </Label>
-            <Input type='select' id='select-plan' name='select-plan'>
-              <option value='basic'>Basic</option>
-              <option value='enterprise'>Enterprise</option>
-              <option value='company'>Company</option>
-              <option value='team'>Team</option>
+            <Input type='select' id='select-plan' name='select-plan' onChange={e => setStatus(e.target.value)} >
+              <option value='basic'>Active</option>
+              <option value='enterprise'>InActive</option>
             </Input>
           </div>
           <Button type='submit' className='me-1' color='primary' >
@@ -256,7 +261,7 @@ const SidebarNewUsers = ({ open, toggleSidebar }) => {
           </Button>
         </Form>}
 
-      {formStatus == 2 &&
+      {/* {formStatus == 2 &&
         <Form onSubmit={handleSubmit(onSubmit)}>
           <div><h1>Guardians details</h1></div>
           <div className='mb-1'>
@@ -353,7 +358,7 @@ const SidebarNewUsers = ({ open, toggleSidebar }) => {
           <Button type='reset' color='secondary' outline onClick={toggleSidebar}>
             Cancel
           </Button>
-        </Form>}
+        </Form>} */}
     </Sidebar>
   )
 }
