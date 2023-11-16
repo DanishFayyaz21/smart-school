0ff528090ffc402f73b176ebc66c46d66ca093df
danish fayyaz <danishfayyaz91@gmail.com>
2023-11-17 00:35:19 +0500

    fixes

--- src/views/teacher/attendence/index.js
+++ src/views/teacher/attendence/index.js
@@ -26,50 +26,28 @@ import {
   UncontrolledButtonDropdown,
   Table,
 } from "reactstrap";
-import { get } from "../../../utility/Axios";
+import { get, post } from "../../../utility/Axios";
 import { useSelector } from "react-redux";
+import moment from "moment/moment";
 
 
 const Attendence = () => {
   const { t } = useTranslation();
 
-  const DecisionData = [
-    {
-      name: "Date of birth",
-      match: "Administrator",
-      watchlist: "International Security Database",
-      status: "Potential Positive",
-    },
-    {
-      name: "Nationality",
-      match: "Administrator",
-      watchlist: "security database",
-      status: "False Positive",
-    },
-    {
-      name: "Gender",
-      match: "Administrator",
-      watchlist: "security database",
-      status: "Potential Positive",
-    },
-    {
-      name: "Country",
-      match: "Administrator",
-      watchlist: "security database",
-      status: "Potential Positive",
-    },
-  ];
 
   const { userData } = useSelector(state => state.auth)
   const [classes, setClasses] = useState([])
-  const [selectedclass, setSelectedclass] = useState()
-
+  const [selectedSubject, setSelectedSubject] = useState()
+  const [attendance, setAttendance] = useState([]);
   const [students, setStudents] = useState([])
+  const [currentClass, setCurrentClass] = useState()
+  const [date, setDate] = useState(moment(new Date()).format("YYYY-MM-DD"))
   const getTeacherClasses = async (id) => {
     try {
       const response = await get(`get-teacher-classes?id=${id}`)
       setClasses(response.data.data)
-      setSelectedclass(response.data.data[0]?._id)
+      setSelectedSubject(response.data.data[0]?._id)
+      setCurrentClass(response.data.data[0]?.classId?._id)
       getClassStudents(response.data.data[0]?.classId?._id)
     } catch (err) {
       console.log("err", err)
@@ -77,6 +55,7 @@ const Attendence = () => {
   }
 
   const getClassStudents = async (id) => {
+
     try {
       const response = await get(`get-class-students/${id}`)
       console.log("rrposne.", response.data.data)
@@ -91,8 +70,51 @@ const Attendence = () => {
   useEffect(() => {
 
     getTeacherClasses(userData?._id)
-
   }, [])
+
+  useEffect(() => {
+    getSpecificDateAttendence(date)
+  }, [selectedSubject])
+  const handleCheckboxChange = (studentId) => {
+
+    if (attendance.includes(studentId)) {
+      setAttendance(attendance.filter((id) => id !== studentId));
+    } else {
+      setAttendance((prevAttendance) => ([
+        ...prevAttendance,
+        studentId,
+      ]))
+    }
+  };
+
+  const getSpecificDateAttendence = async (date) => {
+    try {
+      const response = await get(`get-specific-attendance?subjectId=${selectedSubject}&date=${date}`)
+      if (response.status == 200) {
+        setAttendance(response.data?.attendance.users?.map(item => item?.user))
+      } else {
+        setAttendance([])
+      }
+    } catch (err) {
+      console.log("err", err)
+      setAttendance([])
+    }
+  }
+  const martAttendence = async () => {
+    try {
+      const formData = {
+        classId: currentClass,
+        subjectId: selectedSubject,
+        users: attendance,
+        date
+      }
+      const response = await post("/mark-attendence", formData)
+      console.log(response.data)
+    } catch (err) {
+      console.log("err", err)
+    }
+  }
+
 
 
   return (
@@ -114,8 +136,9 @@ const Attendence = () => {
             <span>{item?.name}</span>
           </button> */}
 
-            <Button className='mt-2 border rounded-2' color={selectedclass == item?._id && "primary"} block onClick={() => {
-              setSelectedclass(item?._id)
+            <Button className='mt-2 border rounded-2' color={selectedSubject == item?._id && "primary"} block onClick={() => {
+              setSelectedSubject(item?._id)
+              setCurrentClass(item?.classId?._id)
               getClassStudents(item?.classId?._id)
             }}>
               <span className='align-middle'>{item?.name + "-" + item?.classId?.name}</span>
@@ -129,10 +152,10 @@ const Attendence = () => {
       {/* button card section */}
 
       <div
-        className="d-flex justify-content-between p-2 rounded"
+        className="d-flex  justify-content-between p-2 rounded"
         style={{ marginTop: "3rem", backgroundColor: "#FFFFFF" }}
       >
-        <div>
+        <div className="d-flex  justify-content-between ">
           <button
             type="button"
             className="flex btn border bg-white hover-bg-danger"
@@ -142,8 +165,21 @@ const Attendence = () => {
             </span>
             <span>{t("Export")}</span>
           </button>
+          <Input
+            className="col-md-4 mx-2 "
+            type="date"
+            value={date}
+            onChange={(e) => {
+              console.log("eeeeeeeee", e.target.value)
+              getSpecificDateAttendence(e.target.value)
+              setDate(moment(e.target.value).format("YYYY-MM-DD"))
+            }}
+          />
         </div>
-        <div className="d-flex gap-5"></div>
+
+        <div className="d-flex gap-5">
+          <Button color="primary" onClick={martAttendence}>Submit</Button>
+        </div>
       </div>
 
       {/* Table card section */}
@@ -156,15 +192,22 @@ const Attendence = () => {
                   <input
                     class="form-check-input"
                     type="checkbox"
-                    value=""
+                    checked={attendance?.length == students?.length}
                     id="flexCheckDefault"
-                  />
+                    onChange={() => {
+                      if (attendance?.length == students?.length) {
+                        setAttendance([])
+                      } else {
+                        setAttendance(students.map(item => item?._id))
+                      }
+                    }}
+                  /> Mart All Present
                 </div>
               </th>
               <th>{t("Roll no")}</th>
               <th>{t("Student Name ")}</th>
               <th>{t("Attendence")}</th>
-              <th style={{ minWidth: '120px' }}> {t("ACTION")}</th>
+              {/* <th style={{ minWidth: '120px' }}> {t("ACTION")}</th> */}
             </tr>
           </thead>
 
@@ -177,32 +220,34 @@ const Attendence = () => {
                       <input
                         class="form-check-input"
                         type="checkbox"
-                        value=""
+                        checked={attendance.includes(item?._id) || false}
+                        onChange={() => handleCheckboxChange(item?._id)}
                         id="flexCheckDefault"
                       />
+                      {console.log("attendance.map(elem => elem == item?._id).length", attendance.includes(item?._id))}
                     </div>
                   </td>
                   <td className="px-2 py-2 ">{item?.rollNumber}</td>
                   <td className="text-capitalize">{item?.user_name}</td>
                   <td>
-                    {true && (
+                    {attendance.includes(item?._id) == true && (
                       <span className="rounded px-1  d-inline-block
-                    d-inline-block" style={{ backgroundColor: " rgba(40, 199, 111, 0.16)", color: " rgba(40, 199, 111, 1)" }}>true</span>
+                    d-inline-block" style={{ backgroundColor: " rgba(40, 199, 111, 0.16)", color: " rgba(40, 199, 111, 1)" }}>Present</span>
                     )}
 
-                    {false && (
+                    {attendance.includes(item?._id) == false && (
                       <span className="rounded px-1  d-inline-block
-                    d-inline-block" style={{ backgroundColor: " rgba(255, 159, 67, 0.16)", color: " rgba(255, 159, 67, 1)" }}>{false}</span>
+                    d-inline-block" style={{ backgroundColor: " rgba(255, 159, 67, 0.16)", color: " rgba(255, 159, 67, 1)" }}>Absent</span>
                     )}
                   </td>
-                  <td className="  gap-1" style={{ fontSize: "17px" }}>
+                  {/* <td className="  gap-1" style={{ fontSize: "17px" }}>
                     <span className="px-1">
                       <TbEdit />
                     </span>
                     <span>
                       <RiDeleteBinLine className="" />
                     </span>
-                  </td>
+                  </td> */}
                 </tr>
               </tbody>
 
@@ -212,10 +257,6 @@ const Attendence = () => {
 
         </Table>
       </Card>
-
-      <div className="d-flex justify-content-center text-center w-100">
-        <span className="m-5 text-center align-self-center">No Student Found</span>
-      </div>
     </Fragment>
 
   )
